d336a7a7b28dff173d4e39f676dc30315bc321f2
SangonomiyaSakunovi <1602274566@qq.com>
2023-05-07 23:21:37 +0800

    The Clock Sync Algorithym in Client

--- Assets/Scripts/Controls/MovePlayerCubeController.cs
+++ Assets/Scripts/Controls/MovePlayerCubeController.cs
@@ -70,7 +70,10 @@ public class MovePlayerCubeController : MonoBehaviour
         {
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothLerpTime * Time.deltaTime);
             AvaterNow.GetComponent<MovePlayerAniController>().AsyncMoveAni(true);
-            MainGameSystem.Instance.SetMiniMapTransPosition(transform);
+            if (isLocalPlayer)
+            {
+                MainGameSystem.Instance.SetMiniMapTransPosition(transform);
+            }           
         }
         else
         {
