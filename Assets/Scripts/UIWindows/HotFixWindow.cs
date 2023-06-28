25fdccfb114c38a13f5b645c1df8e1f4fc68f207
Gaolingx <947770192@qq.com>
2023-06-28 13:32:43 +0800

    hotfixwindow 、gameroot加入ui

--- Assets/Scripts/UIWindows/HotFixWindow.cs
+++ Assets/Scripts/UIWindows/HotFixWindow.cs
@@ -1,10 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.UI;
+ using UnityEngine.EventSystems;
 
 //Developer: SangonomiyaSakunovi
 
-public class HotFixWindow : BaseWindow
-{
-    
-}
+public class HotFixWindow : MonoBehaviour
+ {
+     //获取需要点击的按钮
+    public Button UpdateWindowButton;
+
+    public void UpdateWindowButtonClick()
+     {
+
+        //代码实现按钮的点击
+        ExecuteEvents.Execute(UpdateWindowButton.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+     }
+ }
